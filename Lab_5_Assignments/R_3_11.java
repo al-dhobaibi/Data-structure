72caf4fd969088a36593c2896607ce5ba9212e57
al-dhobaibi <94803066+al-dhobaibi@users.noreply.github.com>
2022-03-22 22:25:58 +0300

    Add files via upload

---
+++ Lab_5_Assignments/R_3_11.java
@@ -0,0 +1,15 @@
+
+
+public class R_3_11 {
+    public int size(){
+        int si=0;
+        if (trailer==null) return si;
+
+        DoublyLinkedList.Node<E> x=header;
+        while (x.getNext()!=trailer){
+            si++;
+            x=x.getNext();
+        }
+        return si;
+    }
+}

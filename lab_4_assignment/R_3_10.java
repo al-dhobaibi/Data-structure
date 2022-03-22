0892a074b855dd0ff64ac388882e3594f4a3b7e6
al-dhobaibi <94803066+al-dhobaibi@users.noreply.github.com>
2022-03-22 21:49:31 +0300

    Add files via upload

---
+++ lab_4_assignment/R_3_10.java
@@ -0,0 +1,16 @@
+package lab_four.lab_four_assignment;
+
+public class R_3_10 {
+
+    public int Size(){
+        int s=0; // s for size
+        if (tail==null) return s;
+        else s++;
+        Node<E> node=tail;
+        while (node.getNext()!=tail){
+            s++;
+            node=node.getNext();
+        }
+        return s;
+    }
+}
---
+++ lab_4_assignment/R_3_7.java
@@ -0,0 +1,17 @@
+package lab_four.lab_four_assignment;
+
+import lab_four.CircularyLinkedList;
+
+
+public class R_3_7 {
+
+    public void addFirst(E e){
+        if (isEmpty()){
+            tail=new CircularyLinkedList.Node<E>(e,null);
+            tail.setNext(tail);
+        }
+        else {
+            tail.next=new CircularyLinkedList.Node<E>(e,tail.getNext()); // rather than creating new node
+        }
+    }
+}
